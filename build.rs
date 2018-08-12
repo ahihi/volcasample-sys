2fd6ab46afa581baa05b2520b54b58612099dade
Miranda Kastemaa <miranda@foldplop.com>
2018-08-13 02:14:46 +0300

    add pattern files

--- build.rs
+++ build.rs
@@ -5,25 +5,25 @@ use std::{error::Error, env, path::PathBuf};
 
 fn build() -> Result<(), Box<Error>> {
     cc::Build::new()
+        .file("vendor/pattern/volcasample_pattern.c")
         .file("vendor/syro/korg_syro_comp.c")
         .file("vendor/syro/korg_syro_func.c")
         .file("vendor/syro/korg_syro_volcasample.c")
-        .compile("syro_volcasample");
+        .compile("volcasample");
 
     let bindings = bindgen::Builder::default()
         .header("vendor/syro/korg_syro_type.h")
         .header("vendor/syro/korg_syro_volcasample.h")
         .header("vendor/syro/korg_syro_comp.h")
         .header("vendor/syro/korg_syro_func.h")
+        .header("vendor/pattern/volcasample_pattern.h")
         .whitelist_type("Endian")
         .whitelist_type("Syro.+")
+        .whitelist_type("VolcaSample_.+")
         .whitelist_function("SyroComp_.+")
         .whitelist_function("SyroFunc_.+")
         .whitelist_function("SyroVolcaSample_.+")
-        .whitelist_var("VOLCASAMPLE_COMP_BLOCK_LEN")
-        .whitelist_var("VOLCASAMPLE_NUM_OF_PATTERN")
-        .whitelist_var("VOLCASAMPLE_NUM_OF_SAMPLE")
-        .whitelist_var("VOLCASAMPLE_PATTERN_SIZE")
+        .whitelist_var("VOLCASAMPLE_.+")
         .rustfmt_bindings(true)
         .generate()
         .map_err(|()| "Failed to generate bindings")?;
